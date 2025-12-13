c1c28067bb470e38fcda7b875b1fc9e7b69c80f3
doccc12 <elkinn2n@gmail.com>
2025-12-13 08:45:36 -0500

    Add files via upload

---
+++ js/animacion.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+
+document.querySelectorAll(".efecto-entrada").forEach(el => {
+    observer.observe(el);
+});
+
+
